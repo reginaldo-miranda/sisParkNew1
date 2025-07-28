9bc848f960f740123e5bece625cc0672483d2c23
Reginaldo Miranda <reginaldobrain@gmail.com>
2025-07-28 20:02:32 -0300

    acerto de tela

--- park/src/pages/Abertos/Abertos.jsx
+++ park/src/pages/Abertos/Abertos.jsx
@@ -1,4 +1,4 @@
-import "../Abertos/Abertos.css";
+/* import "../Abertos/Abertos.css";
 
 export default function Abetos() {
   const placas = [
@@ -23,19 +23,88 @@ export default function Abetos() {
       <h1 className="titulo-serv">Placas Abertas</h1>
       <input type="text" placeholder="entre com a placa" />
 
-        <div className="mostarTela">
-            <div >
+        <div className="mostra-tela" >
+            
                 {placas.map((placa) => (
-                <div key={placa.id} className="card" >
+                <div key={placa.id} >
+                 <div className="card" >
+
+                    <div className="info">
                     <p>Placas:{placa.placas} </p>
                     <p>Cor: {placa.cor} </p>
                     <p>Hora Entrada: {placa.hentrada} </p>
                     <p>Hora Saida: {placa.hsaida} </p>
-                    <button>fechar</button>
+                  </div>
+                    
+
+                    <button className="btn-fechar" >   fechar</button>
                 </div>
+
                 ))}
-            </div>
+
+            
+
+         
         </div>    
+    </>
+  );
+}
+*/
+
+import { useState } from "react";
+import "../Abertos/Abertos.css";
+
+export default function Abertos() {
+  const [filtro, setFiltro] = useState("");
+
+  const placas = [
+    {
+      id: "123",
+      placa: "JCS2345",
+      cor: "verde",
+      hentrada: "15:45",
+      hsaida: "18:42",
+    },
+    {
+      id: "124",
+      placa: "JCS2395",
+      cor: "vermelho",
+      hentrada: "16:45",
+      hsaida: "18:42",
+    },
+  ];
+
+  const placasFiltradas = placas.filter((p) =>
+    p.placa.toLowerCase().includes(filtro.toLowerCase())
+  );
+
+  return (
+    <>
+     <div className="container">
+
+      <h1 className="titulo-serv">Placas Abertas</h1>
+      <input
+        type="text"
+        placeholder="entre com a placa"
+        className="input-placa"
+        value={filtro}
+        onChange={(e) => setFiltro(e.target.value)}
+      />
+
+      <div className="mostra-tela">
+        {placasFiltradas.map((placa) => (
+          <div key={placa.id} className="card">
+            <div className="info">
+              <p>Placa: {placa.placa}</p>
+              <p>Cor: {placa.cor}</p>
+              <p>Hora Entrada: {placa.hentrada}</p>
+              <p>Hora Saída: {placa.hsaida}</p>
+            </div>
+            <button className="btn-fechar">Fechar</button>
+          </div>
+        ))}
+      </div>
+      </div>
     </>
   );
 }
